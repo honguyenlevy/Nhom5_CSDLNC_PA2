cd438f015e084d9a0f1269c649a4eb237805b6e2
Yen Nhi Le <leyennhi112@gmail.com>
2021-12-16 17:48:13 +0700

    Update trigger.sql

--- Application/Script/trigger.sql
+++ Application/Script/trigger.sql
@@ -1,10 +1,7 @@
-﻿use ADB1_5_DATH2
+use ADB1_5_DATH2
 go
-
-update	CT_HOADON
-set ThanhTien = SoLuong * (select GiaSP from SANPHAM where SANPHAM.MaSP = CT_HOADON.MaSP)
-
-create trigger trigger_ThanhTien
+---------------------------------------------------------------------
+create trigger trigger_ThanhTien_updateSL
 on CT_HOADON
 for insert, update, delete	as
 if update (SoLuong)
@@ -15,13 +12,7 @@ begin
 		rollback transaction
 	end
 
-	if exists (select ThanhTien from CT_HOADON where ThanhTien < 0) 
-	begin
-		raiserror (N'Giá sản phẩm không hợp lệ', 10, 1)
-		rollback transaction
-	end
-
-	update	CT_HOADON
+	update CT_HOADON
 	set ThanhTien = cthd.SoLuong * (select GiaSP from SANPHAM where SANPHAM.MaSP = cthd.MaSP)
 	from inserted i, SANPHAM sp, CT_HOADON cthd
 	where 
@@ -29,26 +20,54 @@ begin
 		sp.MaSP = cthd.MaSP
 end
 go
+---------
+
+create trigger trigger_ThanhTien_updateGia
+on SANPHAM
+for insert, update, delete	as
+if update (GiaSP)
+begin
+	if exists (select GiaSP from SANPHAM where GiaSP < 0) 
+	begin
+		raiserror (N'Giá sản phẩm không hợp lệ', 10, 1)
+		rollback transaction
+	end
+
+	update CT_HOADON
+	set ThanhTien = cthd.SoLuong * (select GiaSP from SANPHAM where SANPHAM.MaSP = cthd.MaSP)
+	from inserted i, SANPHAM sp, CT_HOADON cthd
+	where 
+		sp.MaSP = i.MaSP and
+		sp.MaSP = cthd.MaSP
+end
+go
+---------
 
 create trigger trigger_TongTien
 on CT_HOADON 
 for insert, delete, update	as
 begin
-	update HoaDon 
+	update HOADON 
 	set TongTien = (select sum(ThanhTien)
 					from CT_HOADON
 					where CT_HOADON.MaHD = HOADON.MaHD)	+ PhiVanChuyen
 	where 
-		exists (select * from deleted d	 where d.MaHD = HoaDon.MaHD) or
-		exists (select * from inserted i where i.MaHD = HoaDon.MaHD) 
+		exists (select * from deleted d	 where d.MaHD = HOADON.MaHD) or
+		exists (select * from inserted i where i.MaHD = HOADON.MaHD) 
 end
 go
 
-delete from CT_HOADON where MaHD = 'HD000009' and MaSP = 'SP205768'
-insert into	CT_HOADON(MaHD, MaSP, SoLuong, ThanhTien) values (N'HD000009', N'SP205768', 5, 0)
+------------------------------------------------------------------------------
+-- Test
+select * from HOADON where MaHD = N'HD000009'
+select * from CT_HOADON where MaHD = N'HD000009'
+select * from SANPHAM where MaSP = N'sp1002'
 
-select * from HOADON
+insert into SANPHAM (MaSP, TenSP, GiaSP) values (N'sp1002', N'Hershey kisses', 13000)
+insert into CT_HOADON(MaHD, MaSP, SoLuong) values (N'HD000009', N'sp1002', 15)
 
-select * from CT_HOADON
+delete from CT_HOADON where MaHD = N'HD000009' and MaSP = N'sp1002'
+delete from SANPHAM where MaSP = N'sp1002'
 
-select * from SANPHAM
+update CT_HOADON set SoLuong = 7 where MaHD = N'HD000009' and MaSP = N'sp1002'
+update SANPHAM set GiaSP = -10000 where MaSP = N'sp1002'
