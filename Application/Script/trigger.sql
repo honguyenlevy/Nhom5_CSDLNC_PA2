7e6f6eee2e10a0baf77df363e71a60dc5fae4fdd
Yen Nhi Le <leyennhi112@gmail.com>
2021-12-30 23:01:02 +0700

    Update trigger.sql

--- Application/Script/trigger.sql
+++ Application/Script/trigger.sql
@@ -1,10 +1,7 @@
-﻿use ADB1_5_DATH2
+use ADB1_5_DATH2
 go
-
-update	CT_HOADON
-set ThanhTien = SoLuong * (select GiaSP from SANPHAM where SANPHAM.MaSP = CT_HOADON.MaSP)
-
-create trigger trigger_ThanhTien
+---------------------------------------------------------------------
+create trigger trigger_ThanhTien_updateSL
 on CT_HOADON
 for insert, update, delete	as
 if update (SoLuong)
@@ -15,13 +12,27 @@ begin
 		rollback transaction
 	end
 
-	if exists (select ThanhTien from CT_HOADON where ThanhTien < 0) 
+	update CT_HOADON
+	set ThanhTien = cthd.SoLuong * (select GiaSP from SANPHAM where SANPHAM.MaSP = cthd.MaSP)
+	from inserted i, SANPHAM sp, CT_HOADON cthd
+	where 
+		sp.MaSP = i.MaSP and
+		sp.MaSP = cthd.MaSP
+end
+go
+
+create trigger trigger_ThanhTien_updateGia
+on SANPHAM
+for insert, update, delete	as
+if update (GiaSP)
+begin
+	if exists (select GiaSP from SANPHAM where GiaSP < 0) 
 	begin
 		raiserror (N'Giá sản phẩm không hợp lệ', 10, 1)
 		rollback transaction
 	end
 
-	update	CT_HOADON
+	update CT_HOADON
 	set ThanhTien = cthd.SoLuong * (select GiaSP from SANPHAM where SANPHAM.MaSP = cthd.MaSP)
 	from inserted i, SANPHAM sp, CT_HOADON cthd
 	where 
@@ -34,49 +45,29 @@ create trigger trigger_TongTien
 on CT_HOADON 
 for insert, delete, update	as
 begin
-	update HoaDon 
+	update HOADON 
 	set TongTien = (select sum(ThanhTien)
 					from CT_HOADON
 					where CT_HOADON.MaHD = HOADON.MaHD)	+ PhiVanChuyen
 	where 
-		exists (select * from deleted d	 where d.MaHD = HoaDon.MaHD) or
-		exists (select * from inserted i where i.MaHD = HoaDon.MaHD) 
+		exists (select * from deleted d	 where d.MaHD = HOADON.MaHD) or
+		exists (select * from inserted i where i.MaHD = HOADON.MaHD) 
 end
 go
 
-create trigger trigger_DonGia
-on CT_PHIEUDATHANG
-for insert, update, delete	as
-begin
-	if exists (select SoLuongDH from CT_PHIEUDATHANG where SoLuongDH < 0) 
-	begin
-		raiserror (N'Số lượng sản phẩm không hợp lệ', 10, 1)
-		rollback transaction
-	end
+------------------------------------------------------------------------------
+-- Test
+select * from HOADON where MaHD = 2
+select * from CT_HOADON where MaHD = 2
+select * from SANPHAM where MaSP = 1
+select * from SANPHAM where MaSP = 2
+select * from SANPHAM where MaSP = 13568
 
-	if exists (select DonGia from CT_PHIEUDATHANG where DonGia < 0) 
-	begin
-		raiserror (N'Giá sản phẩm không hợp lệ', 10, 1)
-		rollback transaction
-	end
+insert into SANPHAM (TenSP, GiaSP) values (N'Hershey kisses', 13000)
+insert into CT_HOADON(MaHD, MaSP, SoLuong) values (2, 13568, 15)
 
-	update	CT_PHIEUDATHANG
-	set DonGia = (select GiaSP from SANPHAM where SANPHAM.MaSP = ctpdh.MaSP) - 20000
-	from inserted i, SANPHAM sp, CT_PHIEUDATHANG ctpdh
-	where 
-		sp.MaSP = i.MaSP and
-		sp.MaSP = ctpdh.MaSP
-end
-go
+delete from CT_HOADON where MaHD = 2 and MaSP = 13568
+delete from SANPHAM where MaSP = 13568
 
-delete from CT_HOADON where MaHD = 'HD000009' and MaSP = 'SP205768'
-insert into	CT_HOADON(MaHD, MaSP, SoLuong, ThanhTien) values (N'HD000009', N'SP205768', 5, 0)
-
-select * from HOADON
-select * from CT_HOADON
-
-delete from CT_PHIEUDATHANG where MaPDH = 'PDH000111' and MaSP = 'SP166908'
-insert into CT_PHIEUDATHANG(MaPDH, MaSP, SoLuongDH, DonGia) values ('PDH000111', 'SP166908', 10, 0)
-
-select * from SANPHAM where MaSP = 'SP166908'
-select * from CT_PHIEUDATHANG where MaSP = 'SP166908'
+update CT_HOADON set SoLuong = 7 where MaHD = 2 and MaSP = 13568
+update SANPHAM set GiaSP = -10000 where MaSP = 13568
